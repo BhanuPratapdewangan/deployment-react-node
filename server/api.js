f2f16d41848a56180101d73902c65cb90d7e4e19
bh@nu77 <bhanupratap04123@gmail.com>
2023-09-20 14:38:16 +0530

    modify

--- server/api.js
+++ server/api.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5500
 //     credentials:true,            //access-control-allow-credentials:true
 //     optionSuccessStatus:200
 // }
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(urlencoded({extended:true}));
 app.use(express.json());
