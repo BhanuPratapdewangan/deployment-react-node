e13cf70019477e5c8b7d7da31d13c627e2aa628a
bh@nu77 <bhanupratap04123@gmail.com>
2023-09-20 16:37:45 +0530

    modify

--- server/api.js
+++ server/api.js
@@ -8,15 +8,16 @@ const connString = "mongodb://127.0.0.1:27017";
 
 const app = express();
 
-const PORT = process.env.PORT || 5500
+const PORT = process.env.PORT || 3000
 
 // const backend_url = "https://deployment-backend-3in9.onrender.com/";
 
 const corsOptions ={
     origin:'https://deployment-frontend-w7vr.onrender.com', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+    // credentials:true,            //access-control-allow-credentials:true
+    // optionSuccessStatus:200
 }
+
 app.use(cors(corsOptions));
 
 app.use(urlencoded({extended:true}));
@@ -24,10 +25,11 @@ app.use(express.json());
 
 app.options("/", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.sendStatus(204);
+    // res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    // res.sendStatus(204);
   });
+
 
 app.get('/users', (req, res) => {
 
@@ -83,5 +85,5 @@ app.connect((req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Server started to this URL http://localhost:5500`);
+    console.log(`Server is running on port ${PORT} `);
 })
