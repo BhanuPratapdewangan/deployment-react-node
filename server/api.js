0c3fe125c311f54f03e5de84718d6ecf4abc08d6
bh@nu77 <bhanupratap04123@gmail.com>
2023-09-20 13:39:47 +0530

    modify origin

--- server/api.js
+++ server/api.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5500
 // const backend_url = "https://deployment-backend-3in9.onrender.com/";
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://deployment-backend-3in9.onrender.com/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
